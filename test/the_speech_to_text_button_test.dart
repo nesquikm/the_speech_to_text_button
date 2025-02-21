86602df442b6627341f002b382e70aba97269c0f
nesquikm <drapolyuk@gmail.com>
2025-02-21 11:40:13 +0400

    chore: set up GitHub Actions and Dependabot configuration

--- test/the_speech_to_text_button_test.dart
+++ test/the_speech_to_text_button_test.dart
@@ -1,5 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:the_speech_to_text_button/the_speech_to_text_button.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  testWidgets('TheSpeechToTextButton renders using default builder', (
+    tester,
+  ) async {
+    await tester.pumpWidget(const MaterialApp(home: TheSpeechToTextButton()));
+    expect(find.byType(TheSpeechToTextButton), findsOneWidget);
+    expect(find.byIcon(Icons.mic_off), findsOneWidget);
+  });
+
+  testWidgets('TheSpeechToTextButton renders using custom builder', (
+    tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: TheSpeechToTextButton(
+          builder:
+              (BuildContext context, {required state, required onPressed}) =>
+                  TextButton(
+                    onPressed: onPressed,
+                    child: Text(state.type.name),
+                  ),
+        ),
+      ),
+    );
+    expect(find.byType(TheSpeechToTextButton), findsOneWidget);
+    expect(find.byType(TextButton), findsOneWidget);
+    expect(find.text('notReady'), findsOneWidget);
+  });
 }
